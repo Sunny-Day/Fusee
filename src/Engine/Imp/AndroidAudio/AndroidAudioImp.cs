eb5dae67f09745ef3dd22c811a6f3b731d1f9b5f
Christoph Mueller <chrism@g-graphix.de>
2013-04-02 18:28:23 +0200

    Android Audio added

---
+++ src/Engine/Imp/AndroidAudio/AndroidAudioImp.cs
@@ -0,0 +1,49 @@
+﻿#define MP3Warning
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Fusee.Engine
+{
+    public class AndroidAudioImp : IAudioImp
+    {
+        // This is just a stub implementation right now
+        // TODO: implement this!
+        
+        public AndroidAudioImp()
+        {
+        }
+
+        public void OpenDevice()
+        {
+        }
+
+        public void CloseDevice()
+        {
+        }
+
+        public IAudioStream LoadFile(string fileName, bool streaming)
+        {
+            return new AudioStream(fileName, streaming, this);
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void SetVolume(float val)
+        {
+        }
+
+        public float GetVolume()
+        {
+            return 0.42f;
+        }
+
+        public void SetPanning(float val)
+        {
+        }
+    }
+}
---
+++ src/Engine/Imp/AndroidAudio/AudioStream.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Fusee.Engine
+{
+    class AudioStream : IAudioStream
+    {
+        // This is just a stub implementation right now
+        // TODO: implement this!
+        
+        private AndroidAudioImp _audio;
+
+        internal bool IsStream { get; set; }
+
+        public float Volume
+        {
+            get { return 0.42f; }
+            set {  }
+        }
+
+        public float Panning
+        {
+            get { return 0.5f; }
+            set { }
+        }
+
+        public bool Loop
+        {
+            get { return false; }
+            set
+            {
+                
+            }
+        }
+
+        public AudioStream(string fileName, AndroidAudioImp audioCl)
+        {
+
+        }
+
+        public AudioStream(string fileName, bool streaming, AndroidAudioImp audioCl)
+        {
+ 
+        }
+
+
+        public void Dispose()
+        {
+ 
+        }
+
+        public void Play()
+        {
+         }
+
+        public void Play(bool loop)
+        {
+
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
